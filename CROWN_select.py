be8ffe5d09d8dfcf08db28351e003f0575a4b766
Zizhen Lian <zizhlian@umd.edu>
2020-12-12 23:11:49 -0500

    also drop base models before saving

--- CROWN_select.py
+++ CROWN_select.py
@@ -30,37 +30,42 @@ def main():
     config = load_config(crown_args)
     global_eval_config = config["eval_params"]
 
-    if args.load_ada is None:
-        models, model_names = config_modelloader(config, load_pretrain=True)
-
+    if args.models is not None:
+        config["models_path"] = args.models
+    models, model_names = config_modelloader(config, load_pretrain=True)
+    models = [BasePredictor(model) for model in models]
     # Initialize Data
     train_data, test_data = config_dataloader(config, **global_eval_config["loader_params"])
     # Boost
     if args.load_ada is None:
         ada = PretrainedSAMME(train_data,
-                              [BasePredictor(model) for model in models],
+                              models,
                               T=args.iteration)
 
         ada.train()
         # Save the ensemble model
         dump_name = os.path.basename(os.path.splitext(crown_args.config)[0])
         ada.weighted_data = None
-        with open(f'../ada_{dump_name}', "wb") as f:
+        ada.base_predictor_list = None
+        with open(f'../ada_{dump_name}_T{ada.T}', "wb") as f:
             pickle.dump(ada, f)
     else:
         ada = pickle.load(open(args.load_ada, 'rb'))
+        ada.base_predictor_list = [BasePredictor(model) for model in models]
     print(ada.predictor_list)
     print(ada.predictor_weight)
 
     print(f'\n{"Testing":=^20}\n')
     with torch.no_grad():
-
+        ada.base_predictor_list = models
         base_max = 0
         for idx in ada.predictor_list:
             predictor = ada.base_predictor_list[idx]
             correct = 0
             for X, y in test_data:
-                y_pred = predictor.predict(X).argmax(dim=1).to('cpu')
+                X = X.to(ada.device)
+                predictor.model.to(ada.device)
+                y_pred = predictor.predict(X).to('cpu')
                 correct += (y_pred == y).sum()
             if correct > base_max:
                 base_max = correct
@@ -78,6 +83,8 @@ if __name__ == '__main__':
                         help='maximum number of running Adaboost')
     parser.add_argument('--load_ada', '-l', type=str,
                         help='path containing the saved Adaboost model')
+    parser.add_argument('--models', '-m', type=str,
+                        help='path containing the pretrained base model')
 
     args, unknown = parser.parse_known_args()
     # Remove parsed args and pass to CORWN-IBP
