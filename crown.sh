8f13607c3a2e0798781e507d905a8807418425f1
Zizhen Lian <zizhlian@umd.edu>
2021-01-24 11:24:53 -0500

    Server files

---
+++ crown.sh
@@ -0,0 +1,19 @@
+export PYTHONUNBUFFERED=1
+export PYTHONPATH='/mnt/e/Coding/Projects/CMSC828U/boosting-certified-defense/CROWN-IBP'
+
+for (( i = 1; i < 5; i++ )); do
+  python ../CRWON_select_certified_error.py \
+  --config config/mnist_crown_large.json \
+  -e 0.$i \
+  -T 20 \
+  -m "../../828_data/crown-ibp_models/mnist_0.${i}_mnist_crown_large/mnist_crown_large"
+
+  for (( T = 1; T < 20; T++ )); do
+    python ../CRWON_select_certified_error.py \
+    --config config/mnist_crown_large.json \
+    -e 0.$i \
+    -T $T \
+    -m "../../828_data/crown-ibp_models/mnist_0.${i}_mnist_crown_large/mnist_crown_large" \
+    -l "../ada_mnist_crown_large_eps0.${i}_T20"
+  done
+done
