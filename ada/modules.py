dfdb352120764918272befcca0ccf9bd8579113f
Zizhen Lian <zizhlian@umd.edu>
2021-10-02 15:13:02 -0400

    add set_seed for repetitive tests

--- ada/modules.py
+++ ada/modules.py
@@ -1,19 +1,44 @@
-import torch
 from torch import nn
 from torch.nn import functional as F
+
+import pandas as pd
+import torch
+import numpy as np
+import os
+import random
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.enabled = False
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
 
 
 def get_weighted_ce(distribution):
     class WeightedCE(nn.CrossEntropyLoss):
 
         def __init__(self) -> None:
-            # weight=None, size_average=None, ignore_index=-100, reduce=None, reduction'mean'
-            paras = (None, None, -100, None, 'none')
-            super(WeightedCE, self).__init__(*paras)
+            # Default: weight=None, size_average=None, ignore_index=-100,
+            # reduce=None, reduction'mean'
+            paras = {'weight': None,
+                     'size_average': None,
+                     'ignore_index': -100,
+                     'reduce': None,
+                     'reduction': 'none'}
+            super(WeightedCE, self).__init__(**paras)
             self.paras = paras
-            self.distribution = distribution.to('cuda')
+            self.distribution = (distribution / distribution.sum()).to('cuda')
 
-        def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-            return torch.dot(F.nll_loss(F.log_softmax(input, 1), target, *self.paras), self.distribution)
+        def forward(self, input: torch.Tensor,
+                    target: torch.Tensor) -> torch.Tensor:
+            loss = F.nll_loss(F.log_softmax(input, 1), target, **self.paras)
+            return torch.dot(loss, self.distribution)
 
     return WeightedCE
